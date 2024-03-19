fd09b9cfbf4506815430afa8661e293e095deeed
devshehan <shehaan.avishka00@gmail.com>
2024-03-19 20:55:01 +0530

    Implement JWT service

---
+++ src/main/java/com/shehan/authservice/config/JwtService.java
@@ -0,0 +1,82 @@
+package com.shehan.authservice.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    @Value("${secret_key}")
+    private String SECRET_KEY;
+
+    //extract
+    public String extractUsername(String token){
+        return extractClaim(token,Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    // generate
+    public String generateToken(UserDetails userDetails){
+        return generateToken(userDetails, new HashMap<>());
+    }
+
+    public String generateToken(
+            UserDetails userDetails,
+            Map<String, Objects> extraClaims
+    ){
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .claims(extraClaims)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+ 1000*60*24))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    // validation
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (  Objects.equals(username, userDetails.getUsername()) && !isTokenExpired(token) );
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token,Claims::getExpiration);
+    }
+
+
+}
